b2cceedf8bc0c8ca79691e5597c7393962d91d47
Prasit Poudel <prasitpaudel123@gmail.com>
2025-09-21 05:59:13 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Deploying ML Model AS an API with FastAPI
+This repository contains a simple example of how to deploy a machine learning model as an API using FastAPI. The example uses a diabetes prediction model to make predictions based on input data.
+
+Followed the steps from this [tutorial](https://www.youtube.com/watch?v=ZTz26f6XXrQ&list=PLfFghEzKVmjvuSA67LszN1dZ-Dd_pkus6&index=25&pp=iAQB).
---
+++ test_api.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+url = 'http://127.0.0.1:8000/diabetes_prediction'
+
+input_data = {
+    'Pregnancies' : 6,
+    'Glucose' : 148,
+    'BloodPressure' : 72,
+    'SkinThickness' :35,
+    'Insulin' : 0,
+    'BMI' : 33.6,
+    'DiabetesPedigreeFunction' : 0.627,
+    'Age' : 50
+}
+
+input_json = json.dumps(input_data)
+response = requests.post(url, data=input_json)
+
+print(response.text)
